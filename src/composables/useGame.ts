c4ceae46a2fe718f524f042ff9844b05b999f008
EpicMaze <alexrebrikme@gmail.com>
2026-06-13 18:10:10 +0300

    [KAN-11] feat: add game and reputation composables

---
+++ src/composables/useGame.ts
@@ -0,0 +1,26 @@
+import type { ApiError, Game } from '@/api/types'
+import { startGameService } from '@/service/game'
+import { useGameStore } from '@/stores/game'
+import { useMutation } from '@tanstack/vue-query'
+import { useRouter } from 'vue-router'
+
+export const useGame = () => {
+  const store = useGameStore()
+  const router = useRouter()
+
+  const startMutation = useMutation({
+    mutationFn: () => startGameService(),
+    onSuccess: (game: Game) => {
+      store.resetGame()
+      store.setGame(game)
+      router.push('/play')
+    },
+    onError: (error: ApiError) => {
+      console.error('useStartGame', error)
+    },
+  })
+
+  return {
+    startMutation,
+  }
+}
