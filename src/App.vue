3e64d1de1deb5db6608db7acd9b49c7ca2a427ee
EpicMaze <alexrebrikme@gmail.com>
2026-06-13 22:01:59 +0300

    [KAN-36] fix: route to GameOverView when game is over

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
-  import { RouterView } from 'vue-router'
+  import { RouterView, useRouter } from 'vue-router'
   import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+  import { useGameStore } from './stores/game'
+  import { watch } from 'vue'
+
+  const router = useRouter()
+  const store = useGameStore()
+
+  watch(
+    () => store.gameOver.isOver,
+    (isOver) => {
+      if (isOver) router.push('/game-over')
+    },
+  )
 </script>
 
 <template>
